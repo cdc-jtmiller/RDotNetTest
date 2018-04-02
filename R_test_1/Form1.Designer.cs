a13107574b88fd491d81c0cf69a1a4ce5c1b1a5d
zyp9 <zyp9@L320200>
2018-04-02 14:56:00 -0400

    Add project files.

---
+++ R_test_1/Form1.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace R_test_1
+{
+    partial class frm1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtboxFName = new System.Windows.Forms.TextBox();
+            this.btnBrowse = new System.Windows.Forms.Button();
+            this.txtboxRCommands = new System.Windows.Forms.TextBox();
+            this.lblRCmdBox = new System.Windows.Forms.Label();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnSubmit = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtboxFName
+            // 
+            this.txtboxFName.Location = new System.Drawing.Point(12, 12);
+            this.txtboxFName.Name = "txtboxFName";
+            this.txtboxFName.Size = new System.Drawing.Size(391, 20);
+            this.txtboxFName.TabIndex = 1;
+            // 
+            // btnBrowse
+            // 
+            this.btnBrowse.Location = new System.Drawing.Point(430, 12);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(75, 23);
+            this.btnBrowse.TabIndex = 2;
+            this.btnBrowse.Text = "Browse";
+            this.btnBrowse.UseVisualStyleBackColor = true;
+            this.btnBrowse.Click += new System.EventHandler(this.btnBrowse_Click);
+            // 
+            // txtboxRCommands
+            // 
+            this.txtboxRCommands.Location = new System.Drawing.Point(12, 263);
+            this.txtboxRCommands.Multiline = true;
+            this.txtboxRCommands.Name = "txtboxRCommands";
+            this.txtboxRCommands.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txtboxRCommands.Size = new System.Drawing.Size(493, 146);
+            this.txtboxRCommands.TabIndex = 3;
+            this.txtboxRCommands.TextChanged += new System.EventHandler(this.txtboxRCommands_TextChanged);
+            // 
+            // lblRCmdBox
+            // 
+            this.lblRCmdBox.AutoSize = true;
+            this.lblRCmdBox.Location = new System.Drawing.Point(12, 247);
+            this.lblRCmdBox.Name = "lblRCmdBox";
+            this.lblRCmdBox.Size = new System.Drawing.Size(131, 13);
+            this.lblRCmdBox.TabIndex = 4;
+            this.lblRCmdBox.Text = "Enter R commands below:";
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(328, 415);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(75, 23);
+            this.btnClear.TabIndex = 5;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnSubmit
+            // 
+            this.btnSubmit.Location = new System.Drawing.Point(430, 415);
+            this.btnSubmit.Name = "btnSubmit";
+            this.btnSubmit.Size = new System.Drawing.Size(75, 23);
+            this.btnSubmit.TabIndex = 6;
+            this.btnSubmit.Text = "Submit";
+            this.btnSubmit.UseVisualStyleBackColor = true;
+            this.btnSubmit.Click += new System.EventHandler(this.btnSubmit_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 41);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(493, 189);
+            this.dataGridView1.TabIndex = 8;
+            // 
+            // frm1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(531, 448);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btnSubmit);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.lblRCmdBox);
+            this.Controls.Add(this.txtboxRCommands);
+            this.Controls.Add(this.btnBrowse);
+            this.Controls.Add(this.txtboxFName);
+            this.Name = "frm1";
+            this.Text = "EI7_R_test";
+            this.Load += new System.EventHandler(this.frm1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox txtboxFName;
+        private System.Windows.Forms.Button btnBrowse;
+        private System.Windows.Forms.TextBox txtboxRCommands;
+        private System.Windows.Forms.Label lblRCmdBox;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnSubmit;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
---
+++ R_test_1/Form1.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.IO;
+using System.Diagnostics;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using RDotNet;
+
+namespace R_test_1
+{
+
+    public partial class frm1 : Form
+    {
+        public frm1()
+        {
+            InitializeComponent();
+        }
+
+        private void frm1_Load(object sender, EventArgs e)
+        {
+            if (String.IsNullOrEmpty(txtboxRCommands.Text))
+            {
+                btnClear.Enabled = false;
+                btnSubmit.Enabled = false;
+            }
+
+        }
+
+        private void btnBrowse_Click(object sender, EventArgs e)
+        {
+            MyFunctions.InitializeRDotNet();
+            try
+            {
+
+                // import csv file using native R methods
+                //MyFunctions._engine.Evaluate("dataset<-read.csv(file.choose(), header=TRUE, sep = ',', stringsAsFactors = FALSE)");
+                MyFunctions._engine.Evaluate("dataset<-read.csv(file.choose(), sep = ',', stringsAsFactors = FALSE)");
+                //Console.WriteLine("Summary by group: " + results);
+                //var results = MyFunctions._engine.Evaluate("aggregate(. ~GRP, data=dataset, FUN=sum)").AsList();
+                //var filepath = MyFunctions._engine.Evaluate("getwd()");
+                //var normalpath = MyFunctions._engine.Evaluate("eval(parse(text=filepath))");
+                //Console.WriteLine("Path of chosen file is: " + normalpath);
+                //var names = engine.Evaluate("dataset").GetAttributeNames();
+                //txtboxFName.Text = engine.Evaluate("dataset").GetAttribute.Text;
+                //var moreNames = engine.Evaluate("dataset").GetAttribute("row.names");
+
+                //var libraries = MyFunctions._engine.Evaluate("library(lib = .Library)").AsFunction() ;
+                //Console.WriteLine("Path of chosen file is: " + libraries.ToString());
+
+
+                //MyFunctions._engine.Evaluate("ggplot(dataset, aes(X,Y)) + geom_point() + geom_smooth()");
+                var graph = MyFunctions._engine.Evaluate("plot(dataset)");
+                //MyFunctions._engine.Evaluate("qplot(X, Y, data=dataset, geom=c('point', 'smooth'))");
+                
+                // retrieve the data frame
+                DataFrame df = MyFunctions._engine.Evaluate("dataset").AsDataFrame();
+
+
+                for (int i = 0; i < df.ColumnCount; ++i)
+                {
+                    dataGridView1.ColumnCount++;
+                    dataGridView1.Columns[i].Name = df.ColumnNames[i];
+                }
+
+                for (int i = 0; i < df.RowCount; ++i)
+                {
+                    dataGridView1.RowCount++;
+                    dataGridView1.Rows[i].HeaderCell.Value = df.RowNames[i];
+
+                    for (int k = 0; k < df.ColumnCount; ++k)
+                    {
+                        dataGridView1[k, i].Value = df[i, k];
+
+                    }
+
+                }
+
+
+            }
+
+            catch
+            {
+                MessageBox.Show(@"Sonmething went wrong.");
+            }
+
+            // put R code execute here.
+            
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            txtboxRCommands.Text = "";
+        }
+
+        private void txtboxRCommands_TextChanged(object sender, EventArgs e)
+        {
+            if (String.IsNullOrEmpty(txtboxRCommands.Text))
+            {
+                btnClear.Enabled = false;
+                btnSubmit.Enabled = false;
+            }
+            else
+            {
+                btnClear.Enabled = true;
+                btnSubmit.Enabled = true;
+            }
+        }
+
+        private void btnSubmit_Click(object sender, EventArgs e)
+        {
+            
+            try
+            {
+                // get contents of datagrid
+
+
+                //MyFunctions._engine.Evaluate("\"" + txtboxRCommands.Text + "\"");
+                //Console.WriteLine(MyFunctions._engine.Evaluate("aggregate(. ~GRP, data=dataset, FUN=sum)"));
+            }
+
+            catch
+            {
+                MessageBox.Show(@"Sonmething went wrong.");
+            }
+
+        }
+
+        private void frm1_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            //MyFunctions._engine.Dispose();
+        }
+    }
+
+
+    public static class MyFunctions
+    {
+        public static REngine _engine;
+        internal static void InitializeRDotNet()
+        {
+            try
+            {
+
+                //string rHome = @"C:\Program Files\R\R-3.4.3";
+                //string rPath = @"C:\Program Files\R\R-3.4.3\bin\x64";
+                ////string rPath = Path.Combine(rHome, @"bin\x64");
+                //REngine.SetEnvironmentVariables(rPath, rHome);
+                //_engine = REngine.GetInstance();
+
+                REngine.SetEnvironmentVariables();
+                _engine = REngine.GetInstance();
+
+                _engine.Initialize();
+
+                foreach (string sRSession in MyFunctions._engine.Evaluate("sessionInfo()").AsCharacter())
+                {
+                    Console.WriteLine("Current session info: " + sRSession);
+                }
+
+
+                //foreach (string sPackages in MyFunctions._engine.Evaluate("installed.packages(.Library)").AsCharacter())
+                //{
+                //    Console.WriteLine("Installed packages on local machine are: " + sPackages);
+                //}
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error Initializing RDotNet: " + ex.Message);
+            }
+        }
+    }
+}
